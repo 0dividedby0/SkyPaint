ac6e2e0a47b7c87efb960e5848c69ba2f4e2438a
0dividedby0 <jason.halcomb@wsu.edu>
2018-03-03 14:13:40 -0800

    Connection updated for main menu

--- SkyPaint/MainMenuController.swift
+++ SkyPaint/MainMenuController.swift
@@ -22,4 +22,50 @@ class MainMenuController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        guard let connectedKey = DJIProductKey(param: DJIParamConnection) else {
+            NSLog("Error creating the connectedKey")
+            return;
+        }
+        
+        DJISDKManager.keyManager()?.startListeningForChanges(on: connectedKey, withListener: self, andUpdate: { (oldValue: DJIKeyedValue?, newValue : DJIKeyedValue?) in
+            if newValue != nil {
+                if newValue!.boolValue {
+                    // At this point, a product is connected so we can show it.
+                    
+                    // UI goes on MT.
+                    DispatchQueue.main.async {
+                        self.productConnected()
+                    }
+                }
+            }
+        })
+        
+        DJISDKManager.keyManager()?.startListeningForChanges(on: connectedKey, withListener: self, andUpdate: { (oldValue: DJIKeyedValue?, newValue : DJIKeyedValue?) in
+            if newValue != nil {
+                if newValue!.boolValue {
+                    // At this point, a product is connected so we can show it.
+                    
+                    // UI goes on MT.
+                    DispatchQueue.main.async {
+                        self.productConnected()
+                    }
+                }
+            }
+        })
+    }
+    
+    func productConnected() {
+        guard let newProduct = DJISDKManager.product() else {
+            NSLog("Product is connected but DJISDKManager.product is nil -> something is wrong")
+            return;
+        }
+        
+        let connectionString = "Model: \((newProduct.model)!)" + "\n" + "Status: Product Connected"
+        
+        let alert = UIAlertController(title: "New Connection!", message: connectionString, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
 }

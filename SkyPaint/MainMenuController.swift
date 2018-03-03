c1c6beb37cd0a4fe5f85484837017a2cc501fd40
0dividedby0 <jason.halcomb@wsu.edu>
2018-03-03 13:18:33 -0800

    added main menu

---
+++ SkyPaint/MainMenuController.swift
@@ -0,0 +1,25 @@
+//
+//  MainMenuController.swift
+//  SkyPaint
+//
+//  Created by Jason Halcomb on 3/3/18.
+//  Copyright © 2018 SkyPaint. All rights reserved.
+//
+
+import UIKit
+import DJISDK
+import VideoPreviewer
+
+class MainMenuController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

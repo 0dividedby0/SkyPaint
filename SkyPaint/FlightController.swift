b96e29f136726077ca287bce33fd1aa8eef5b884
0dividedby0 <jason.halcomb@wsu.edu>
2018-03-03 16:23:57 -0800

    Added LiveView

--- SkyPaint/FlightController.swift
+++ SkyPaint/FlightController.swift
@@ -10,16 +10,25 @@ import UIKit
 import DJISDK
 import VideoPreviewer
 
-class FlightController: UIViewController {
-
-    @IBOutlet weak var ModelLabel: UILabel!
-    @IBOutlet weak var BatteryLabel: UILabel!
-    @IBOutlet weak var StatusLabel: UILabel!
+class FlightController: UIViewController, DJIVideoFeedListener, DJICameraDelegate {
+    
+    @IBOutlet var liveView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (DJISDKManager.product() == nil) {
+            let alert = UIAlertController(title: "No Connection", message: "", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+        let camera = (DJISDKManager.product() as! DJIAircraft).camera
+        camera!.delegate = self
+        
+        self.setupVideoPreviewer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +36,30 @@ class FlightController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupVideoPreviewer() {
+        VideoPreviewer.instance().setView(self.liveView)
+        
+        DJISDKManager.videoFeeder()?.primaryVideoFeed.add(self, with: nil)
+        
+        VideoPreviewer.instance().start()
+    }
+    
+    func resetVideoPreview() {
+        VideoPreviewer.instance().unSetView()
+        DJISDKManager.videoFeeder()?.primaryVideoFeed.remove(self)
+    }
+    
+    func videoFeed(_ videoFeed: DJIVideoFeed, didUpdateVideoData rawData: Data) {
+        let videoData = rawData as NSData
+        let videoBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: videoData.length)
+        
+        videoData.getBytes(videoBuffer, length: videoData.length)
+        VideoPreviewer.instance().push(videoBuffer, length: Int32(videoData.length))
+    }
+    
+    func camera(_ camera: DJICamera, didUpdate systemState: DJICameraSystemState) {
+        
+    }
+    
 }
 
